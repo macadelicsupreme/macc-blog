14d53f84c3858796da54ca69dfe03591866789d2
macc <macadelicsupreme@outlook.com>
2024-12-10 14:37:25 -0500

    addded "styling. need to fix blog cards so  that posts show"

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,10 +1,6 @@
-// Import the glob loader
-import { glob } from "astro/loaders";
-// Import utilities from `astro:content`
 import { z, defineCollection } from "astro:content";
 // Define a `loader` and `schema` for each collection
 const blog = defineCollection({
-    loader: glob({ pattern: '**/[^_]*.md', base: "./src/blog" }),
     schema: z.object({
         title: z.string(),
         pubDate: z.date(),
@@ -14,7 +10,7 @@ const blog = defineCollection({
             url: z.string(),
             alt: z.string()
         }),
-        tags: z.array(z.string())
+        tags: z.array(z.string()).optional()
     })
 });
 // Export a single `collections` object to register your collection(s)
